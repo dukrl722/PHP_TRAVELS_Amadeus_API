54f01de76d7d743cecaa969bd510f63ecbd978ba
Eduardo <dukrl222@hotmail.com>
2023-11-23 10:38:51 -0300

    Call more APIs

--- src/ApiService.php
+++ src/ApiService.php
@@ -70,6 +70,11 @@ class ApiService
 
             curl_close($curl);
 
+            foreach ($response['data'] as $key => $value) {
+                $response['data'][$key] = $this->AmadeusHotelDetailAPI($value);
+                $response['data'][$key]['address'] = $this->GoogleMapsAPI($value);
+            }
+
             return $response;
 
         } catch (Exception $exception) {
@@ -77,6 +82,129 @@ class ApiService
                 'status' => 500,
                 'message' => $exception->getMessage()
             ]);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function AmadeusHotelDetailAPI(array $hotel): array
+    {
+        try {
+
+            $url = $_ENV['API_URL_V3'] . $_ENV['API_HOTEL_DETAIL_URL'] . '?hotelIds=' . $hotel['hotelId'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => [
+                    'Accept: application/vnd.amadeus+json',
+                    'Authorization: Bearer ' . $_SERVER['token']
+                ]
+            ]);
+
+            $response = json_decode(curl_exec($curl), true);
+
+            curl_close($curl);
+
+            $hotel['price'] = 0.00;
+            $hotel['price_per_night'] = 0.00;
+            $hotel['currency'] = 'USD';
+            $hotel['booking_currency'] = 'USD';
+            $hotel['service_fee'] = 0.00;
+
+            if (!empty($response['data'])) {
+                $hotel['price'] = floatval($response['data'][0]['offers'][0]['price']['total']);
+                $hotel['price_per_night'] = floatval($response['data'][0]['offers'][0]['price']['total']);
+                $hotel['currency'] = $response['data'][0]['offers'][0]['price']['currency'];
+                $hotel['booking_currency'] = $response['data'][0]['offers'][0]['price']['currency'];
+
+                if (!empty($response['data'][0]['offers'][0]['price']['base'])) {
+                    $hotel['service_fee'] = number_format(
+                        floatval(
+                            $response['data'][0]['offers'][0]['price']['total'] - $response['data'][0]['offers'][0]['price']['base']
+                        )
+                    , 2);
+                }
+            }
+
+            return $hotel;
+
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+    }
+
+    /**
+     * @author Eduardo da Silva
+     * @since 23/11/2023
+     * @return mixed
+     *
+     * Don't work
+     */
+    private function AmadeusRatingDetailAPI()
+    {
+        if (empty($_SERVER['token'])) {
+            $this->AmadeusAuth();
+        }
+
+        $url = $_ENV['API_URL_V2'] . $_ENV['API_HOTEL_RATING_URL'] . '?hotelIds=MUDXBAIR';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => [
+                'Accept: application/vnd.amadeus+json',
+                'Authorization: Bearer ' . $_SERVER['token']
+            ]
+        ]);
+
+        $response = json_decode(curl_exec($curl), true);
+
+        curl_close($curl);
+
+        return $response;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function GoogleMapsAPI(array $data)
+    {
+        try {
+
+            $url = $_ENV['API_GOOGLE_MAPS_URL']
+                . '?latlng='
+                . $data['geoCode']['latitude']
+                . ','
+                . $data['geoCode']['longitude']
+                . '&key='
+                . $_ENV['API_GOOGLE_MAPS_KEY'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ]);
+
+            $response = json_decode(curl_exec($curl), true);
+
+            curl_close($curl);
+
+            if ($response['status'] == 'OK') {
+                return $response['results'][0]['formatted_address'];
+            }
+
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
         }
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -4,15 +4,22 @@ function hotelsResource(array $data) {
 
     $response = [];
 
-    foreach ($data as $value) {
+    foreach ($data as $key => $value) {
         $response[] = [
             'hotel_id' => $value['hotelId'],
-            'img' => null,
+            'img' => '',
             'name' => $value['name'],
-            'location' => '',
-            'address' => 'Dubai - AE',
+            'location' => '', // x
+            'address' => $value['address'],
+            'stars' => 5, // x
+            'rating' => 100, // x
             'latitude' => $value['geoCode']['latitude'],
             'longitude' => $value['geoCode']['longitude'],
+            'price' => $value['price'],
+            'price_per_night' => $value['price_per_night'],
+            'currency' => $value['currency'],
+            'booking_currency' => $value['booking_currency'],
+            'service_fee' => floatval($value['service_fee']),
             'suplier_name' => 'amadeus_hotels',
             'redirect' => '',
             'booking_data' => []
